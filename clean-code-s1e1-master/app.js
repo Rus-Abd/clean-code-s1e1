57a7ff62cb0f77a07cb0441fe75c893552e5c48f
ruslan <ruslan.abdallah2000@gmail.com>
2021-11-26 17:54:09 +0300

    refactor: adjust HTML-formatting according to rule 1.1 in html-and-css-extended.md

--- clean-code-s1e1-master/app.js
+++ clean-code-s1e1-master/app.js
@@ -77,10 +77,10 @@ var editTask = function () {
   var editInput = listItem.querySelector('input[type=text]');
   var label = listItem.querySelector('label');
   var editBtn = listItem.querySelector('.edit');
-  var containsClass = listItem.classList.contains('editMode');
-  //If class of the parent is .editmode
+  var containsClass = listItem.classList.contains('task-edit');
+  //If class of the parent is .task-edit
   if (containsClass) {
-    //switch to .editmode
+    //switch to .task-edit
     //label becomes the inputs value.
     label.innerText = editInput.value;
     editBtn.innerText = 'Edit';
@@ -89,8 +89,8 @@ var editTask = function () {
     editBtn.innerText = 'Save';
   }
 
-  //toggle .editmode on the parent.
-  listItem.classList.toggle('editMode');
+  //toggle .task-edit on the parent.
+  listItem.classList.toggle('task-edit');
 };
 
 //Delete task.
